702e6df79ceaba8a80d7a131e437246c8d5bdfe3
Jose Manuel Lezcano Fresno <reymidas76@yahoo.es>
2022-12-03 12:10:01 +0100

    first exercise resolved

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,9 +1,34 @@
 package com.codely.demo
 
+import java.time.LocalDate
+import java.time.Period
+import kotlin.system.exitProcess
+
 class Codelyber {
     fun greet() = "Welcome to kotlin skeleton!!!"
 }
 
-fun main(args: Array<String>) {
-    println(Codelyber().greet())
+fun main() {
+    println("Please enter a date with the format <yyyy-MM-dd>")
+    supportNulableString(readLine()).takeUnless {
+        it.isNullOrEmpty() || it.isNullOrBlank()
+    }?.let {
+        LocalDate.parse(it)
+    }.apply {
+        // para hacer mutaciones
+        if (this == null) {
+            println("The date is not valid")
+            exitProcess(1)
+        }
+    }?.also {
+        println("You wrote $it")
+    }.run {
+        with(Period.between(this, LocalDate.now())){
+            println("The different between the date you wrote and today is ${this.years}")
+        }
+    }
+
 }
+
+
+fun supportNulableString(line: String?) = line
